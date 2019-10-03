5224c5227c7af3b4ae7e1135d874ef513ca53881
mcnallc2 <mcnallc2@tcd.ie>
2019-10-03 19:10:57 +0100

    removing packages and adding updated client server

--- Assignment1/server.js
+++ Assignment1/server.js
@@ -11,9 +11,6 @@ app.listen(port, () => console.log(`Example app listening on port ${port
 
 function data(req, res) {
     let location = req.params.location;
-    //res.json( { result : location });
-    //console.log('recieved::' + location);
-
     console.log("Weather request for:", location)
 
     let url = "http://api.openweathermap.org/data/2.5/forecast?q=" + location + "&APPID=3e2d927d4f28b456c6bc662f34350957";
@@ -22,17 +19,9 @@ function data(req, res) {
     let prom = fetch(url)
     prom.then(response => response.json() )
     .then(response => {
-        parseWeather(response)
-        weather = response.weather
-        console.log(weather) }
-    )
-    .catch(function(error){
-      console.log(error);
+        res.json(response)
     })
-}
-
-function parseWeather(info){
-    let kelvin = 273.15;
-
-    weather = info.weather
+    .catch(function(error){
+        console.log(error);
+    })
 }
